5b11157033f1a9690aa9975eedd80bbe991ecac7
Dmitry Kouznetsov <dmitry.kouznetsov@nentgroup.net>
2020-04-20 14:52:40 +0200

    overlay

--- content.js
+++ content.js
@@ -1,1 +1,5 @@
-console.log("hi");
+var overlay=document.createElement("div");
+overlay.id="overlay";
+
+overlay.insertAdjacentHTML('afterbegin', `<span class='class123'>Basketty</span>`);
+document.body.appendChild(overlay);
