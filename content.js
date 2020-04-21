81cf7028bab79a3aaab9117fb1a7a522f87da0a8
Dmitry Kouznetsov <dmitry.kouznetsov@nentgroup.net>
2020-04-21 15:03:31 +0200

    print the cookie

--- content.js
+++ content.js
@@ -5,9 +5,15 @@ overlay.insertAdjacentHTML('afterbegin', `
 <div>
     <span class='class123'>Basketty</span>
 </div>
+<div>
+    <span class='class123'>cookie=</span>
+    <span id='basketty::cookie' class='class123'>?</span>
+</div>
 `);
 document.body.appendChild(overlay);
 
 chrome.runtime.sendMessage({msg: 'hi from content'}, function(resp) {
+    document.getElementById("basketty::cookie").innerHTML="asdf";
+    console.log(document.getElementById("basketty::cookie").innerHTML);
     console.log(resp);
 });
